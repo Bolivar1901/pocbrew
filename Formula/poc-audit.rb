6e9c60cbd127b371656025b9e153dd0761d485d6
Bolivar1901 <bvbenitez5@hotmail.com>
2026-03-18 20:03:45 -0500

    Update poc-audit.rb

--- Formula/poc-audit.rb
+++ Formula/poc-audit.rb
@@ -1,1 +1,11 @@
+class PocAudit < Formula
+  desc "PoC auditoría Homebrew"
+  homepage "https://example.com"
+  url "https://example.com/fake.tar.gz"
+  version "1.0"
 
+  def install
+    system "echo", "POC: ejecución de código desde Homebrew" 
+    system "touch", "/tmp/poc_homebrew_evidence"
+  end
+end
